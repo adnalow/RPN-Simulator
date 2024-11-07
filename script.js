0a02d16bece1815e81151aa42197c252f642de28
KimMathew <kimmathewbautista11@gmail.com>
2024-11-07 08:46:13 +0800

    Added pressed button

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function showMainContent() {
+function showMainContent(buttonElement) {
     document.querySelector('.to-postfix').classList.remove('hidden');
     document.getElementById('start').style.display = 'none';
+
+    buttonElement.src = 'assets/images/active_button.png';
+
+    setTimeout(() => {
+        buttonElement.src = 'assets/images/button.png';
+    }, 200); // Adjust timing for a realistic arcade press effect
 }
 
